482928f68d9de3aee86effec854ea85107b2df6a
Ignacio Alonso de la Cruz Garcia <ignacio.cruzg@bancoazteca.com>
2023-01-09 17:07:09 -0600

    Adición de operaciones raiz y exponencial

--- CalculadoraOk.py
+++ CalculadoraOk.py
@@ -1,10 +1,14 @@
+import numpy as np
+
 def calculator():
     # Create dictionary
     menu = {
-        '1': 'Suma',
-        '2': 'Resta',
-        '3': 'Multiplicación',
-        '4': 'División',
+        '1': 'Sumar',
+        '2': 'Restar',
+        '3': 'Multiplicar',
+        '4': 'Dividir',
+        '5': 'Raíz n',
+        '6': 'Exponente n',
         '0': 'Salir'
     }
 
@@ -20,55 +24,87 @@ def calculator():
             print(f'{key}. {menu[key]}')
 
         # Request operation
-        option = input("\n¿Qué operación deseas realizar? (1/2/3/4/0): ")
+        option = input("\n¿Qué operación deseas realizar? (1 / 2 / 3/ 4 / 5 / 6 / 0): ")
 
         # Validate option 0, close app
         if option == '0':
             exit()
 
         if option in menu:
-            # Validate float number
-            try:
-                num1 = float(input("\nIngresa el primer número: "))
-                num2 = float(input("Ingresa el segundo número: "))
-            except ValueError:
-                print("Entrada no valida")
-        
             # Do operations
             if option == '1':
-                print("El resultado de la suma es: ", suma(num1, num2) )
+                print("SUMA > El resultado es: ", sumar())
             elif option =='2':
-                print("El resultado de la resta es: ", resta(num1, num2))
+                print("RESTA > El resultado es: ", restar())
             elif option == '3':
-                print("El resultado de la  multiplicación es: ", multiplicacion(num1, num2))
+                print("MULTIPLICACIÓN > El resultado es: ", multiplicar())
             elif option == '4':
-                print("El resultado de la división es: ", division(num1, num2))
+                print("DIVIDIR > El resultado es: ", dividir())
+            elif option == '5':
+                print("RAIZ CUADRADA > El resultado es: ", raiz())
+            elif option == '6':
+                print("EXPONENCIAL > El resultado es: ", exponencial())
         else:
             print ("Opción no valida.")
         
         # Call function again
         again()
 
-def suma(a, b):
-    # Do sum
-    return a + b
+def sumar():
+    # Validate float number
+    try:
+        a = float(input("\nIngresa el primer número: "))
+        b = float(input("Ingresa el segundo número: "))
+        return a + b
+    except ValueError:
+        print("Entrada no valida")
 
-def resta(a, b):
-    # Do abstract
-    return a - b
+def restar():
+    # Validate float number
+    try:
+        a = float(input("\nIngresa el primer número: "))
+        b = float(input("Ingresa el segundo número: "))
+        return a - b
+    except ValueError:
+        print("Entrada no valida")
 
-def multiplicacion(a, b):
-    # Do multiply
-    return a * b
+def multiplicar():
+    # Validate float number
+    try:
+        a = float(input("\nIngresa el primer número: "))
+        b = float(input("Ingresa el segundo número: "))
+        return a * b
+    except ValueError:
+        print("Entrada no valida.")
 
-def division(a, b):
-    # Validate division for 0
-    if a == 0:
-        # If division is for 0 return error msg
-        return "No es posible la división entre 0"
-    else:
-        # Do division
-        return a / b
+def dividir():
+    try:
+        a = float(input("\nIngresa el primer número: "))
+        b = float(input("Ingresa el segundo número: "))
+        if a == 0:
+            # If division is for 0 return error msg
+            return "No es posible la división entre 0"
+        else:
+            # Do division
+            return a / b
+    except ValueError:
+        print("Entrada no valida.")
+    
+def raiz ():
+    try:
+        a = float(input("\nIngresa el primer número: "))
+        return np.sqrt(a)
+    except ValueError:
+        print("Entrada no valida.")
+    
+def exponencial():
+    # Validate float number
+    try:
+        a = float(input("\nIngresa el número: "))
+        b = float(input("Ingresa la exponencia: "))
+        return np.power(a, b)
+    except ValueError:
+        print("Entrada no valida")
 
 def again():
     # Ask if need to do other operation and validate response
